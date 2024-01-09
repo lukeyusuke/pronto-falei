01df07230e9f0057158f4bd9811f5273df7a2d98
lukeyusuke <lukedev09@gmail.com>
2024-01-09 13:40:38 -0300

    feat(config): config webpack
    - Add support for Sass module loading

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+   module: {
+      rules: [
+         {
+            test: /\.scss$/,
+            use: [
+               'style-loader', 'css-loader', 'sass-loader'
+            ]
+         }, 
+      ]
+   }
+}
