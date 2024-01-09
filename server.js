0c21f0dec6d8fe2eda56df25b1d81b16ffb777a0
lukeyusuke <lukedev09@gmail.com>
2024-01-09 13:52:25 -0300

    fetch(config): config server
    - import dependencies
    - listen for port 3000

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import routes from './routes.js';
+const app = express();
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.set('views', [
+   path.resolve(__dirname, 'src', 'views', 'Home')
+])
+
+app.set('view engine', 'ejs');
+app.use(routes);
+
+app.listen(3000, () => {
+   console.log('Servidor http://localhost:3000');
+})
