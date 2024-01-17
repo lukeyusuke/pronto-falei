890d01a709e16a7fe5fb82f0ee2f40de2d4d317e
lukeyusuke <lukedev09@gmail.com>
2024-01-17 15:47:24 -0300

    feat(user): form validation

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,7 +6,6 @@ export const homeIndex = (req, res) => {
 
 export const homeSendEmail = (req, res) => {
    const { nome, email, mensagem } = req.body;
-   console.log(email);
 
    let transporter = nodeMailer.createTransport({
       service: 'gmail',
@@ -27,12 +26,47 @@ export const homeSendEmail = (req, res) => {
       text: mensagem,
    }
 
-   transporter.sendMail(configEmail, (err, info) => {
-      try {
-         res.status(200).json();
-         console.log(info);
-      } catch(err){
-         console.log(err)
+   const validateForm = () => {
+      let error;
+
+      const validateName = (nome) => {
+         const regex = /[0-9]/;
+         return regex.test(nome);
       }
-   })
+
+      const validateEmail = (email) => {
+         const regex = /\S+@\S+\.\S+/;
+         return regex.test(email)
+      }
+
+      const checkWhiteSpace = (valor) => {
+         const regex = /\S/;
+         return regex.test(valor);
+      }
+
+      if(validateName(nome) || (nome.length <= 3) || (!checkWhiteSpace(nome))){
+         error = 'Nome inválido';
+         res.status(400).json({ error });
+
+      } else if(!validateEmail(email)){
+         error = 'Email inválido'
+         res.status(400).json({ error });
+
+      } else if(!checkWhiteSpace(mensagem)){
+         error = 'Digite sua mensagem'
+         res.status(400).json({ error });
+
+      } else {
+         transporter.sendMail(configEmail, (err, info) => {
+            try {
+               res.status(200).json();
+               console.log(info);
+            } catch(err){
+               console.log(err)
+            }
+         })
+      }
+   }
+
+   validateForm();
 }
--- src/js/home.js
+++ src/js/home.js
@@ -17,6 +17,9 @@ const darkLightMode = () => {
 
 const sendEmail = () => {
    const form = document.querySelector('.talk-us-content__form')
+   const inputBox = document.querySelectorAll('.input-box');
+   const errorMessage = document.querySelectorAll('.error-message');
+   const successMessage = document.querySelector('.success-message')
 
    form.addEventListener('submit', (e) => {
       e.preventDefault();
@@ -29,7 +32,36 @@ const sendEmail = () => {
          },
          body: JSON.stringify(Object.fromEntries(formData))
       }).then((response) => {
-         console.log(response);
+         const errorMessages = [...errorMessage.values()];
+         const inputs = [...inputBox.values()];
+
+         successMessage.classList.remove('active');
+         errorMessages.map((error) => {
+            error.classList.remove('active');
+         })
+
+         if(response.status === 400){
+            return response.json().then((data) => {
+               switch(data.error) {
+                  case 'Nome inválido':
+                     errorMessage[0].classList.add('active');
+                  break;
+                  case 'Email inválido':
+                     errorMessage[1].classList.add('active');
+                  break;
+                  case 'Digite sua mensagem':
+                     errorMessage[2].classList.add('active');
+                  break;
+               } 
+            });
+         } else {
+            successMessage.classList.add('active');
+            inputs.map((input) => {
+               input.value = ' ';
+            })
+         }
+      }).catch((err) => {
+         console.log(err);
       })
    })
 }
