66f16ece470330b3e232298f2df5845de61d203c
lukeyusuke <lukedev09@gmail.com>
2024-01-18 15:39:56 -0300

    feat(structure): reorganizing the folder structure

--- routes.js
+++ routes.js
@@ -2,7 +2,11 @@ import express from 'express';
 const route = express.Router();
 
 import { homeIndex, homeSendEmail } from './src/controllers/homeController.js';
+import { loginPage } from './src/controllers/loginController.js';
 
 route.get('/', homeIndex);
-route.post('/', homeSendEmail)
+route.post('/', homeSendEmail);
+
+route.get('/login', loginPage);
+
 export default route;
