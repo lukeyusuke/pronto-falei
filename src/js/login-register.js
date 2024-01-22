f5ce08996bca62c05375319aa421f4842d0c5fa9
lukeyusuke <lukedev09@gmail.com>
2024-01-22 18:18:57 -0300

    feat(login): add tabs-navigation logic to login page

--- src/js/login-register.js
+++ src/js/login-register.js
@@ -1,19 +1,49 @@
-const tabNavigation = () => {
+const TabNavigation = () => {
    const tabLinks = [...document.querySelector('.tab-links').children];
-   const contents = [...document.querySelector('.tab-content').children];
+   const contents = [...document.querySelector('.tab-content').children]; 
    const openTab = document.querySelector('[data-open]');
 
    const hideAllTabContent = () => {
       contents.forEach(section => {
-         section.style.display = "none"
+         section.style.display = "none";
       })
    }
 
+   const removeAllActiveClass = () => {
+      tabLinks.forEach(tab => {
+         tab.className = tab.className.replace(" active", "");
+      })
+   }
+
+   const showCurrentTab = (id) => {
+      const tabContent = document.querySelector('#' + id);
+      tabContent.style.display = "block";
+   }
+
+   const selectTab = (e) => {
+      hideAllTabContent();
+      removeAllActiveClass();
+
+      const target = e.currentTarget;
+      showCurrentTab(target.dataset.id);
+
+      target.className += " active"
+   }
+
+   const listenChanges = () => {
+      tabLinks.forEach(tab => {
+         tab.addEventListener('click', selectTab);
+      })
+   }
+   
    const init = () => {
       hideAllTabContent();
+      listenChanges();
+
+      openTab.click();
    }
 
    return init();
 }
 
-tabNavigation();
+TabNavigation();
