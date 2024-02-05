6b900fc567c6176a0c546134619cbc5d7f1c0307
lukeyusuke <lukedev09@gmail.com>
2024-02-05 15:50:50 -0300

    style(login): separating login and signup page 2

--- routes.js
+++ routes.js
@@ -2,12 +2,15 @@ import express from 'express';
 const route = express.Router();
 
 import { homeIndex, homeSendEmail } from './src/controllers/homeController.js';
-import { loginPage, searchAllUsers, loginUser, createUser, updateUser, deleteUser } from './src/controllers/loginController.js';
+import { loginPage, searchAllUsers, loginUser, updateUser, deleteUser } from './src/controllers/loginController.js';
+import { registerPage, createUser } from './src/controllers/registerController.js';
 
 route.get('/', homeIndex);
 route.post('/', homeSendEmail);
 
 route.get('/login', loginPage);
+route.get('/register', registerPage)
+
 route.get('/login/users', searchAllUsers); // Listar todos os usuários
 route.post('/register', createUser); // Criar novo usuário
 route.post('/login', loginUser); // Fazer Login com o usuário
